d2adc543bba3ba8a2a6524d8771e55459315e1a1
Ori-i0n <orionconner00@gmail.com>
2019-10-21 12:13:10 +0530

    Restructured logos dictionary and wrapped the logo printing process inside a function

--- logos.py
+++ logos.py
@@ -9,17 +9,37 @@ logos = {
            [1, 1, 2, 2, 2, 2, 2, 2],
            [1, 1, 2, 2, 2, 2, 2, 2],
            [0, 0, 2, 2, 3, 2, 0, 0],
-           [0, 0, 2, 2, 2, 2, 0, 0]]
+           [0, 0, 2, 2, 2, 2, 0, 0]],
+
+    "js": [[0, 0, 0, 0, 0, 0, 0, 0],
+           [0, 0, 0, 0, 0, 0, 0, 0],
+           [0, 0, 0, 0, 0, 0, 2, 2],
+           [1, 1, 1, 1, 1, 1, 2, 2],
+           [1, 1, 2, 2, 2, 2, 2, 2],
+           [1, 1, 2, 2, 2, 2, 2, 2],
+           [0, 0, 2, 2, 3, 2, 0, 0],
+           [0, 0, 2, 2, 2, 2, 0, 0]],
+
+    "chars": {
+                "py": ["#", "L", "@"]
+
+    }
 }
 
-chars = "#", "L", "@"
-pylogo = logos.get("py")
 
-for i in range(8):
-    for j in range(8):
-        cell_val = pylogo[i][j]
-        if cell_val != 0:
-            print(chars[cell_val - 1], end=" ")
-        else:
-            print(" ", end=" ")
-    print()
+def print_logo(lang_name):
+    """Prints logos of programming languages."""
+    chars = logos.get("chars").get(lang_name)
+    logo = logos.get(lang_name)
+
+    for i in range(8):
+        for j in range(8):
+            cell_val = logo[i][j]
+            if cell_val != 0:
+                print(chars[cell_val - 1], end=" ")
+            else:
+                print(" ", end=" ")
+        print()
+
+
+print_logo("py")
